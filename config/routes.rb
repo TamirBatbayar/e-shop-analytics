0e6a3f881080a82700b10243b406423c8258366b
TamirBatbayar <tamir@keis-software.com>
2015-11-26 12:39:13 +0900

    New Dimension Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  resources :books
+  # root to: 'visitors#index'
+  root to: 'books#index'
   devise_for :users
   resources :users
 end
